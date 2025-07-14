4ecaac7b8d0d69ddb8ca5156c0e4b6d3eac1ab4f
svin24 <svin.main.24@gmail.com>
2025-07-14 19:06:32 +0300

    add brave browser vscode and various other utilities

--- build_files/build.sh
+++ build_files/build.sh
@@ -9,15 +9,24 @@ set -ouex pipefail
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
+# RPM Fusion repos
 dnf5 -y install \
         https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-"$(rpm -E %fedora)".noarch.rpm \
         https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-"$(rpm -E %fedora)".noarch.rpm
 
+# vscode repo
+rpm --import https://packages.microsoft.com/keys/microsoft.asc
+echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
 
-dnf5 remove -y firefox
-dnf5 install -y tmux neovim ripgrep fd-find
+# Brave
+curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+curl -fsSLo /etc/apt/sources.list.d/brave-browser-release.sources https://brave-browser-apt-release.s3.brave.com/brave-browser.sources
 
-dnf5 install -y haruna okular krita
+dnf5 install -y tmux neovim ripgrep fd-find distrobox cargo rustup golang
+dnf5 install -y haruna okular 
+dnf5 install -y yt-dlp ffmpeg-full steam
+dnf5 install -y code brave-browser
+
 # Use a COPR Example:
 #
 # dnf5 -y copr enable ublue-os/staging
